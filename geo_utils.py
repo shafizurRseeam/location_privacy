0395447fd83c3f8467405ba7ee3f72b918556b8a
ShafizurRahmanSeeam <seeamrit22@gmail.com>
2024-03-13 18:02:21 -0400

    Added comment to check

--- geo_utils.py
+++ geo_utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 EARTH_RADIUS = 6371000
 
@@ -11,5 +12,13 @@ def y_x_to_latitude_longitude(y, x):
     longitude = np.degrees(x / EARTH_RADIUS)
     latitude = np.degrees(y / EARTH_RADIUS)
     return latitude, longitude
+
+def get_direction(loc1, loc2):
+    dLon = (loc2[1] - loc1[1])
+
+    y = math.sin(math.radians(dLon)) * math.cos(math.radians(loc2[0]))
+    x = math.cos(math.radians(loc1[0])) * math.sin(math.radians(loc2[0])) - math.sin(math.radians(loc1[0])) * math.cos(math.radians(loc2[0])) * math.cos(math.radians(dLon))
+    brng = math.atan2(y, x)
+    return brng
 
 #test to see changes
