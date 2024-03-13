4947f6ac29b31d021d8fe38b3f67f94963fd5c6e
ShafizurRahmanSeeam <seeamrit22@gmail.com>
2024-03-13 18:02:59 -0400

    Added comment to check

--- data_processor.py
+++ data_processor.py
@@ -2,8 +2,10 @@ import pandas as pd
 import os
 from mechanism_laplace import planar_laplace_mechanism
 from mechanism_staircase import planar_staircase_mechanism
+from mechanism_staircase_intermediate import intermediate, only_reported_locations
 
-def process_file_laplace(file_path, epsilon, base_directory_output, noise_laplace):
+def process_file_laplace(file_path, epsilon, base_directory_output,
+                          noise_laplace):
 
 
     df = pd.read_csv(file_path)
@@ -22,7 +24,8 @@ def process_file_laplace(file_path, epsilon, base_directory_output, noise_laplac
 
 
 
-def process_file_staircase(file_path, epsilon, base_directory_output, noise_staircase):
+def process_file_staircase(file_path, epsilon, base_directory_output, 
+                           noise_staircase):
 
 
     df = pd.read_csv(file_path)
@@ -37,4 +40,26 @@ def process_file_staircase(file_path, epsilon, base_directory_output, noise_stai
 
     perturbed_locations.to_csv(csv_file_path_out, index=False)
 
+    print(f"Processed and saved: {output_csv_file_name}")
+
+
+
+def process_file_staircase_intermediate(file_path, epsilon, base_directory_output,  
+                                        noise_staircase, noise_staircase_bounded,
+                                        delta):
+    
+
+
+ 
+    df = pd.read_csv(file_path)
+    
+    intermediate_dataset = intermediate(df.copy(), noise_staircase_bounded)
+    perturbed_locations = only_reported_locations(intermediate_dataset.copy(), noise_staircase, delta)
+
+    
+    original_file_name = os.path.splitext(os.path.basename(file_path))[0]
+    output_csv_file_name = f"{original_file_name}_our_{epsilon}.csv"
+    csv_file_path_out = os.path.join(base_directory_output, output_csv_file_name)
+
+    perturbed_locations.to_csv(csv_file_path_out, index=False)
     print(f"Processed and saved: {output_csv_file_name}")
