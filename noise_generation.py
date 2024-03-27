a068a5166f74019644b4a06b879c1b44ebca5cec
ShafizurRahmanSeeam <seeamrit22@gmail.com>
2024-03-27 09:36:57 -0400

    updated

--- noise_generation.py
+++ noise_generation.py
@@ -2,16 +2,18 @@ import numpy as np
 import random
 import scipy.special
 
-
+def set_random_seed(seed=42):
+    """Set the random seed for both numpy and random modules to a fixed value."""
+    np.random.seed(seed)
+    random.seed(seed)
 
 def pdf_values(epsilon, x_interval, L):
-    """Compute normalized PDF values for a given range using the given epsilon."""
+    set_random_seed()
     b = (1 - np.exp(-epsilon)) / x_interval
     intervals = int(L / x_interval)
-    positions = np.linspace(0, L - x_interval, intervals)  # directly create the positions array
+    positions = np.linspace(0, L - x_interval, intervals)
     unnormalized_pdf_samples = b * np.exp(-epsilon * positions)
     
-    # Normalize
     area = np.sum(unnormalized_pdf_samples) * x_interval
     normalized_pdf_samples = unnormalized_pdf_samples / area
     return positions, normalized_pdf_samples
@@ -19,31 +21,26 @@ def pdf_values(epsilon, x_interval, L):
 def draw_from_pdf(epsilon, x_interval, L):
     positions, normalized_pdf_samples = pdf_values(epsilon, x_interval, L)
     
-    # Compute the cumulative distribution
     cdf = np.cumsum(normalized_pdf_samples)
     cdf /= cdf[-1]
     
-    # Draw a random number between 0 and 1
     r = np.random.rand()
     
-    # Find the index where the random number fits into the CDF
     index = np.searchsorted(cdf, r)
     
-    # Get radial distance from the selected interval
     start_of_interval = positions[index]
     end_of_interval = start_of_interval + x_interval
     random_r_value = np.random.uniform(start_of_interval, end_of_interval)
     
-    # Generate the random theta value
     theta = np.random.uniform(0, 2 * np.pi)
     
-    # Convert r and theta to x and y
     x = random_r_value * np.cos(theta)
     y = random_r_value * np.sin(theta)
     
     return x, y
 
 def generate_laplace_noise_samples(number_samples, epsilon):
+    set_random_seed()
     samples = []
     for _ in range(number_samples):
         theta = np.random.uniform(0, 2 * np.pi)
@@ -59,5 +56,3 @@ def generate_staircase_noise_samples(epsilon, x_interval, L, number_samples):
         x, y = draw_from_pdf(epsilon, x_interval, L)
         samples.append((x, y))
     return samples
-
-
