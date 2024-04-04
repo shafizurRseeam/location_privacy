f84102255a43494fdf911d3baf472e093b27abbd
ShafizurRahmanSeeam <seeamrit22@gmail.com>
2024-04-04 17:38:25 -0400

    updated

--- perturbation_uci.py
+++ perturbation_uci.py
@@ -5,27 +5,35 @@ commands = [
     r'python main_laplace.py --input_dir "C:\Users\ss6365\Desktop\location_privacy_final\uci\data\utility" --output_dir "C:\Users\ss6365\Desktop\location_privacy_final\uci\temporary"',
     r'python move_files.py "C:\Users\ss6365\Desktop\location_privacy_final\uci\temporary" "C:\Users\ss6365\Desktop\location_privacy_final\uci\perturbed\{mechanism}\{epsilon}" laplace laplace',
     r'python merge_csv_in_subdirs.py "C:\Users\ss6365\Desktop\location_privacy_final\uci\perturbed\laplace"',
-    r'python main_laplace_delta.py --input_dir "C:\Users\ss6365\Desktop\location_privacy_final\uci\data\utility" --output_dir "C:\Users\ss6365\Desktop\location_privacy_final\uci\temporary"',
-    r'python move_files.py "C:\Users\ss6365\Desktop\location_privacy_final\uci\temporary" "C:\Users\ss6365\Desktop\location_privacy_final\uci\perturbed\{mechanism}\{epsilon}" laplace_delta_5 laplaceDelta',
-    r'python merge_csv_in_subdirs.py "C:\Users\ss6365\Desktop\location_privacy_final\uci\perturbed\laplace_delta_5"',
+
+    # r'python main_laplace_delta.py --input_dir "C:\Users\ss6365\Desktop\location_privacy_final\uci\data\utility" --output_dir "C:\Users\ss6365\Desktop\location_privacy_final\uci\temporary"',
+    # r'python move_files.py "C:\Users\ss6365\Desktop\location_privacy_final\uci\temporary" "C:\Users\ss6365\Desktop\location_privacy_final\uci\perturbed\{mechanism}\{epsilon}" laplace_delta_5 laplaceDelta',
+    # r'python merge_csv_in_subdirs.py "C:\Users\ss6365\Desktop\location_privacy_final\uci\perturbed\laplace_delta_5"',
+
     r'python main_staircase.py --input_dir "C:\Users\ss6365\Desktop\location_privacy_final\uci\data\utility" --output_dir "C:\Users\ss6365\Desktop\location_privacy_final\uci\temporary"',
     r'python move_files.py "C:\Users\ss6365\Desktop\location_privacy_final\uci\temporary" "C:\Users\ss6365\Desktop\location_privacy_final\uci\perturbed\{mechanism}\{epsilon}" staircase staircase',
     r'python merge_csv_in_subdirs.py "C:\Users\ss6365\Desktop\location_privacy_final\uci\perturbed\staircase"',
+
     r'python main_staircase_intermediate.py --input_dir "C:\Users\ss6365\Desktop\location_privacy_final\uci\data\utility" --output_dir "C:\Users\ss6365\Desktop\location_privacy_final\uci\temporary" --min_r_value 50 --delta 5',
     r'python move_files.py "C:\Users\ss6365\Desktop\location_privacy_final\uci\temporary" "C:\Users\ss6365\Desktop\location_privacy_final\uci\perturbed\{mechanism}\{epsilon}" our_bl_50_delta_5 our',
     r'python merge_csv_in_subdirs.py "C:\Users\ss6365\Desktop\location_privacy_final\uci\perturbed\our_bl_50_delta_5"',
+
     r'python main_staircase_intermediate.py --input_dir "C:\Users\ss6365\Desktop\location_privacy_final\uci\data\utility" --output_dir "C:\Users\ss6365\Desktop\location_privacy_final\uci\temporary" --min_r_value 50 --delta 10',
     r'python move_files.py "C:\Users\ss6365\Desktop\location_privacy_final\uci\temporary" "C:\Users\ss6365\Desktop\location_privacy_final\uci\perturbed\{mechanism}\{epsilon}" our_bl_50_delta_10 our',
     r'python merge_csv_in_subdirs.py "C:\Users\ss6365\Desktop\location_privacy_final\uci\perturbed\our_bl_50_delta_10"',
+
     r'python main_staircase_intermediate.py --input_dir "C:\Users\ss6365\Desktop\location_privacy_final\uci\data\utility" --output_dir "C:\Users\ss6365\Desktop\location_privacy_final\uci\temporary" --min_r_value 50 --delta 20',
     r'python move_files.py "C:\Users\ss6365\Desktop\location_privacy_final\uci\temporary" "C:\Users\ss6365\Desktop\location_privacy_final\uci\perturbed\{mechanism}\{epsilon}" our_bl_50_delta_20 our',
     r'python merge_csv_in_subdirs.py "C:\Users\ss6365\Desktop\location_privacy_final\uci\perturbed\our_bl_50_delta_20"',
+
     r'python main_staircase_intermediate.py --input_dir "C:\Users\ss6365\Desktop\location_privacy_final\uci\data\utility" --output_dir "C:\Users\ss6365\Desktop\location_privacy_final\uci\temporary" --min_r_value 200 --delta 5',
     r'python move_files.py "C:\Users\ss6365\Desktop\location_privacy_final\uci\temporary" "C:\Users\ss6365\Desktop\location_privacy_final\uci\perturbed\{mechanism}\{epsilon}" our_bl_200_delta_5 our',
     r'python merge_csv_in_subdirs.py "C:\Users\ss6365\Desktop\location_privacy_final\uci\perturbed\our_bl_200_delta_5"',
-    r'python main_staircase_intermediate.py --input_dir "C:\Users\ss6365\Desktop\location_privacy_final\uci\data\utility" --output_dir "C:\Users\ss6365\Desktop\location_privacy_final\uci\temporary" --min_r_value 100 --delta 5',
-    r'python move_files.py "C:\Users\ss6365\Desktop\location_privacy_final\uci\temporary" "C:\Users\ss6365\Desktop\location_privacy_final\uci\perturbed\{mechanism}\{epsilon}" our_bl_100_delta_5 our',
-    r'python merge_csv_in_subdirs.py "C:\Users\ss6365\Desktop\location_privacy_final\uci\perturbed\our_bl_100_delta_5"',
+
+    # r'python main_staircase_intermediate.py --input_dir "C:\Users\ss6365\Desktop\location_privacy_final\uci\data\utility" --output_dir "C:\Users\ss6365\Desktop\location_privacy_final\uci\temporary" --min_r_value 100 --delta 5',
+    # r'python move_files.py "C:\Users\ss6365\Desktop\location_privacy_final\uci\temporary" "C:\Users\ss6365\Desktop\location_privacy_final\uci\perturbed\{mechanism}\{epsilon}" our_bl_100_delta_5 our',
+    # r'python merge_csv_in_subdirs.py "C:\Users\ss6365\Desktop\location_privacy_final\uci\perturbed\our_bl_100_delta_5"',
+
     r'python main_staircase_intermediate.py --input_dir "C:\Users\ss6365\Desktop\location_privacy_final\uci\data\utility" --output_dir "C:\Users\ss6365\Desktop\location_privacy_final\uci\temporary" --min_r_value 20 --delta 5',
     r'python move_files.py "C:\Users\ss6365\Desktop\location_privacy_final\uci\temporary" "C:\Users\ss6365\Desktop\location_privacy_final\uci\perturbed\{mechanism}\{epsilon}" our_bl_20_delta_5 our',
     r'python merge_csv_in_subdirs.py "C:\Users\ss6365\Desktop\location_privacy_final\uci\perturbed\our_bl_20_delta_5"',
