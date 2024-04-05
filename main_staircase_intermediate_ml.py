5628c811a4a7caeb9c47341a675dc85780e30698
ShafizurRahmanSeeam <seeamrit22@gmail.com>
2024-04-05 18:18:11 -0400

    updated

--- main_staircase_intermediate_ml.py
+++ main_staircase_intermediate_ml.py
@@ -53,7 +53,7 @@ def main_staircase_intermediate_ml():
     # Ensure output directory exists
     os.makedirs(base_directory_output, exist_ok=True)
     
-    epsilon_values = [0.1, 0.2, 0.5, 1, 2, 3, 4, 5]
+    epsilon_values = [0.1]
  
 
     for epsilon in epsilon_values:
