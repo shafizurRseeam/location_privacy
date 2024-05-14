fa909aeff482ed58a76a9f8439c2e9255f386120
ShafizurRahmanSeeam <seeamrit22@gmail.com>
2024-05-14 11:50:22 -0400

    updated

--- encode_all.py
+++ encode_all.py
@@ -2,10 +2,12 @@ import subprocess
 
 # Define the commands to run, corrected for apparent path errors
 commands = [
-   r'python encode_for_ml.py "C:\Users\ss6365\Desktop\location_privacy_final\geolife\perturbed_averaged" "C:\Users\ss6365\Desktop\location_privacy_final\geolife\machine_learning\attack1"',
-   r'python encode_for_ml.py "C:\Users\ss6365\Desktop\location_privacy_final\tdrive\perturbed_averaged" "C:\Users\ss6365\Desktop\location_privacy_final\tdrive\machine_learning\attack1"',
-   r'python encode_for_ml.py "C:\Users\ss6365\Desktop\location_privacy_final\uci\perturbed_averaged" "C:\Users\ss6365\Desktop\location_privacy_final\uci\machine_learning\attack1"',
-   r'python encode_for_ml.py "C:\Users\ss6365\Desktop\location_privacy_final\collected\perturbed_averaged" "C:\Users\ss6365\Desktop\location_privacy_final\collected\machine_learning\attack1"',
+   #r'python encode_for_ml.py "C:\Users\ss6365\Desktop\location_privacy_final\geolife\perturbed_averaged" "C:\Users\ss6365\Desktop\location_privacy_final\geolife\machine_learning\attack1"',
+   #r'python encode_for_ml.py "C:\Users\ss6365\Desktop\location_privacy_final\tdrive\perturbed_averaged" "C:\Users\ss6365\Desktop\location_privacy_final\tdrive\machine_learning\attack1"',
+   #r'python encode_for_ml.py "C:\Users\ss6365\Desktop\location_privacy_final\uci\perturbed_averaged" "C:\Users\ss6365\Desktop\location_privacy_final\uci\machine_learning\attack1"',
+   #r'python encode_for_ml.py "C:\Users\ss6365\Desktop\location_privacy_final\collected\perturbed_averaged" "C:\Users\ss6365\Desktop\location_privacy_final\collected\machine_learning\attack1"',
+   r'python encode_for_ml.py "C:\Users\ss6365\Desktop\location_privacy_final\tracebased\perturbed_averaged_10" "C:\Users\ss6365\Desktop\location_privacy_final\tracebased\machine_learning\attack1"',
+
 ]
 
 # Execute each command sequentially
